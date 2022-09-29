1c1b33d6fba778a6b4cc6d26256ed52cc396df0b
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2022-09-29 10:19:10 +0700

    Update values

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-export const ONE_MILLISECOND = 1
-export const ONE_SECOND = 1000
-export const ONE_MINUTE = 60000
-export const ONE_HOUR = 3600000
-export const ONE_DAY = 86400000
-export const ONE_WEEK = 604800000
-export const ONE_MONTH = 2592000000
+export const ONE_SECOND_MS = 1000
+export const ONE_MINUTE_MS = 60000
+export const ONE_HOUR_MS = 3600000
+export const ONE_DAY_MS = 86400000
+export const ONE_WEEK_MS = 604800000
+export const ONE_MONTH_MS = 2592000000
+
+export const ONE_SECOND_S = 1
+export const ONE_MINUTE_S = 60
+export const ONE_HOUR_S = 3600
+export const ONE_DAY_S = 86400
+export const ONE_WEEK_S = 604800
+export const ONE_MONTH_S = 2592000
