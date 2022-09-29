d165184fc957954c6c45b926121a8adbe4d57a8e
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2022-09-29 09:52:18 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const ONE_MILLISECOND = 1
+export const ONE_SECOND = 1000
+export const ONE_MINUTE = 60000
+export const ONE_HOUR = 3600000
+export const ONE_DAY = 86400000
+export const ONE_WEEK = 604800000
+export const ONE_MONTH = 2592000000
